03bf80d17b428eaaecebd11337be3c061b9289c0
BoredGuy_ <106572634+Sunburn28@users.noreply.github.com>
2024-02-20 22:32:29 -0600

    Update EdadG.cs

--- EdadG.cs
+++ EdadG.cs
@@ -16,5 +16,19 @@ namespace Ejercicios4
         {
             InitializeComponent();
         }
+
+        private void EdadG_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Calcular_Click(object sender, EventArgs e)
+        {
+            int[] edades = txtEdades.Text.Split(',')
+                                           .Select(s => int.Parse(s.Trim()))
+                                           .ToArray();
+            var resultados = CalculadoraEdades.Calcular(edades);
+            MessageBox.Show($"La media de las edades es: {resultados.media}\nLa edad mayor es: {resultados.edadMayor}\nLa edad menor es: {resultados.edadMenor}", "Resultados");
+        }
     }
-}
+ }
